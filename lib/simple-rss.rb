8f704a6af74fb7dc7682951a5ba0b52d018e639b
Aqab Bin Talal <ab@dutchcoastsoftware.com>
2010-01-13 14:28:04 +0100

    Refactoring and speeding up tests

--- lib/simple-rss.rb
+++ lib/simple-rss.rb
@@ -2,125 +2,114 @@ require 'cgi'
 require 'time'
 
 class SimpleRSS
-  VERSION = "1.2.3"
-  
-	attr_reader :items, :source
-	alias :entries :items
+  VERSION = "1.2.4"
 
-	@@feed_tags = [
-		:id,
-		:title, :subtitle, :link,
-		:description, 
-		:author, :webMaster, :managingEditor, :contributor,
-		:pubDate, :lastBuildDate, :updated, :'dc:date',
-		:generator, :language, :docs, :cloud,
-		:ttl, :skipHours, :skipDays,
-		:image, :logo, :icon, :rating,
-		:rights, :copyright,
-		:textInput, :'feedburner:browserFriendly',
-		:'itunes:author', :'itunes:category'
-	]
+  attr_reader :items, :source
+  alias :entries :items
 
-	@@item_tags = [
-		:id,
-		:title, :link, :'link+alternate', :'link+self', :'link+edit', :'link+replies',
-		:author, :contributor,
-		:description, :summary, :content, :'content:encoded', :comments,
-		:pubDate, :published, :updated, :expirationDate, :modified, :'dc:date',
-		:category, :guid,
-		:'trackback:ping', :'trackback:about',
-		:'dc:creator', :'dc:title', :'dc:subject', :'dc:rights', :'dc:publisher',
-		:'feedburner:origLink'
-	]
+  @@feed_tags = [
+    :id,
+    :title, :subtitle, :link,
+    :description,
+    :author, :webMaster, :managingEditor, :contributor,
+    :pubDate, :lastBuildDate, :updated, :'dc:date',
+    :generator, :language, :docs, :cloud,
+    :ttl, :skipHours, :skipDays,
+    :image, :logo, :icon, :rating,
+    :rights, :copyright,
+    :textInput, :'feedburner:browserFriendly',
+    :'itunes:author', :'itunes:category'
+  ]
 
-	def initialize(source, options={})
-		@source = source.respond_to?(:read) ? source.read : source.to_s
-		@items = Array.new
+  @@item_tags = [
+    :id,
+    :title, :link, :'link+alternate', :'link+self', :'link+edit', :'link+replies',
+    :author, :contributor,
+    :description, :summary, :content, :'content:encoded', :comments,
+    :pubDate, :published, :updated, :expirationDate, :modified, :'dc:date',
+    :category, :guid,
+    :'trackback:ping', :'trackback:about',
+    :'dc:creator', :'dc:title', :'dc:subject', :'dc:rights', :'dc:publisher',
+    :'feedburner:origLink'
+  ]
+
+  def initialize(source, options={})
+    @source = source.respond_to?(:read) ? source.read : source.to_s
+    @items = Array.new
     @options = Hash.new.update(options)
-    
-		parse
-	end
 
-	def channel() self end
-	alias :feed :channel
+    parse
+  end
 
-	class << self
-		def feed_tags
-			@@feed_tags
-		end
-		def feed_tags=(ft)
-			@@feed_tags = ft
-		end
+  def channel() self end
+  alias :feed :channel
 
-		def item_tags
-			@@item_tags
-		end
-		def item_tags=(it)
-			@@item_tags = it
-		end
+  class << self
+    def feed_tags
+      @@feed_tags
+    end
+    def feed_tags=(ft)
+      @@feed_tags = ft
+    end
 
-		# The strict attribute is for compatibility with Ruby's standard RSS parser
-		def parse(source, options={})
-			new source, options
-		end
-	end
+    def item_tags
+      @@item_tags
+    end
+    def item_tags=(it)
+      @@item_tags = it
+    end
 
-	private
+    # The strict attribute is for compatibility with Ruby's standard RSS parser
+    def parse(source, options={})
+      new source, options
+    end
+  end
 
-	def parse
-	  raise SimpleRSSError, "Poorly formatted feed" unless @source =~ %r{<(channel|feed).*?>.*?</(channel|feed)>}mi
-	  
-		# Feed's title and link
-		feed_content = $1 if @source =~ %r{(.*?)<(rss:|atom:)?(item|entry).*?>.*?</(rss:|atom:)?(item|entry)>}mi
-		
-		@@feed_tags.each do |tag|
-			if feed_content && feed_content =~ %r{<(rss:|atom:)?#{tag}(.*?)>(.*?)</(rss:|atom:)?#{tag}>}mi
-				nil
-			elsif feed_content && feed_content =~ %r{<(rss:|atom:)?#{tag}(.*?)\/\s*>}mi
-				nil
-			elsif @source =~ %r{<(rss:|atom:)?#{tag}(.*?)>(.*?)</(rss:|atom:)?#{tag}>}mi
-				nil
-			elsif @source =~ %r{<(rss:|atom:)?#{tag}(.*?)\/\s*>}mi
-				nil
-			end
-			
-			if $2 || $3
+  private
+
+  def parse
+    raise SimpleRSSError, "Poorly formatted feed" unless m = %r{<(channel|feed).*?>.*?</(channel|feed)>}mi.match(@source)
+
+    # Feed's title and link
+    feed_content = m[1] if  m = %r{(.*?)<(rss:|atom:)?(item|entry).*?>.*?</(rss:|atom:)?(item|entry)>}mi.match(@source)
+
+    @@feed_tags.each do |tag|
+      m = (feed_content && ( %r{<(rss:|atom:)?#{tag}(.*?)>(.*?)</(rss:|atom:)?#{tag}>}mi.match(feed_content) ||
+                             %r{<(rss:|atom:)?#{tag}(.*?)\/\s*>}mi.match(feed_content) )) ||
+          %r{<(rss:|atom:)?#{tag}(.*?)>(.*?)</(rss:|atom:)?#{tag}>}mi.match(@source) ||
+          %r{<(rss:|atom:)?#{tag}(.*?)\/\s*>}mi.match(@source)
+
+      if m && (m[2] || m[3])
         tag_cleaned = clean_tag(tag)
-        instance_variable_set("@#{ tag_cleaned }", clean_content(tag, $2, $3))
+        instance_variable_set("@#{ tag_cleaned }", clean_content(tag, m[2],m[3]))
         self.class.send(:attr_reader, tag_cleaned)
-			end
-		end
+      end
+    end
 
-		# RSS items' title, link, and description
-		@source.scan( %r{<(rss:|atom:)?(item|entry)([\s][^>]*)?>(.*?)</(rss:|atom:)?(item|entry)>}mi ) do |match|
-			item = Hash.new
-			@@item_tags.each do |tag|
-			  if tag.to_s.include?("+")
-			    tag_data = tag.to_s.split("+")
-			    tag = tag_data[0]
-			    rel = tag_data[1]
-			    
-  				if match[3] =~ %r{<(rss:|atom:)?#{tag}(.*?)rel=['"]#{rel}['"](.*?)>(.*?)</(rss:|atom:)?#{tag}>}mi
-            nil
-  				elsif match[3] =~ %r{<(rss:|atom:)?#{tag}(.*?)rel=['"]#{rel}['"](.*?)/\s*>}mi
-  				  nil
-  				end
-  				item[clean_tag("#{tag}+#{rel}")] = clean_content(tag, $3, $4) if $3 || $4
-		    else
-  				if match[3] =~ %r{<(rss:|atom:)?#{tag}(.*?)>(.*?)</(rss:|atom:)?#{tag}>}mi
-  					nil
-  				elsif match[3] =~ %r{<(rss:|atom:)?#{tag}(.*?)/\s*>}mi
-  					nil
-  				end
-  				item[clean_tag(tag)] = clean_content(tag, $2, $3) if $2 || $3
-				end
-			end
-			
-      # ,hack to fix blogspot atom feed links pointing to comments issue
+    # RSS items' title, link, and description
+    @source.scan( %r{<(rss:|atom:)?(item|entry)([\s][^>]*)?>(.*?)</(rss:|atom:)?(item|entry)>}mi ) do |match|
+      item = Hash.new
+      @@item_tags.each do |tag|
+        if tag.to_s.include?("+")
+          tag_data = tag.to_s.split("+")
+          tag = tag_data[0]
+          rel = tag_data[1]
+
+          m = %r{<(rss:|atom:)?#{tag}(.*?)rel=['"]#{rel}['"](.*?)>(.*?)</(rss:|atom:)?#{tag}>}mi.match(match[3]) ||
+              %r{<(rss:|atom:)?#{tag}(.*?)rel=['"]#{rel}['"](.*?)/\s*>}mi.match(match[3])
+          item[clean_tag("#{tag}+#{rel}")] = clean_content(tag, m[3], m[4]) if m && (m[3] || m[4])
+        else
+          m = %r{<(rss:|atom:)?#{tag}(.*?)>(.*?)</(rss:|atom:)?#{tag}>}mi.match(match[3]) ||
+              %r{<(rss:|atom:)?#{tag}(.*?)/\s*>}mi.match(match[3])
+          item[clean_tag(tag)] = clean_content(tag, m[2],m[3]) if m && (m[2] || m[3])
+        end
+      end
+
+      # Hack to fix blogspot atom feed links pointing to comments issue
       # Looks like the code here is just taking the FIRST link tag and using
       # the href from that. In Blogspot atom feeds, this tends to be the link
       # to the comments - not what we want.
-      # The RFC (http://www.ietf.org/rfc/rfc4287.txt) states that 
+      # The RFC (http://www.ietf.org/rfc/rfc4287.txt) states that
       # 'atom:link elements MAY have a "rel" attribute that indicates the link
       # relation type.  If the "rel" attribute is not present, the link
       # element MUST be interpreted as if the link relation type is
@@ -130,36 +119,36 @@ class SimpleRSS
       if item[:'link+alternate']
         item[:link] = item[:'link+alternate']
       end
-      
+
       def item.method_missing(name, *args) self[name] end
-      
-			@items << item
-		end
 
-	end
+      @items << item
+    end
 
-	def clean_content(tag, attrs, content)
-		content = content.to_s
-		case tag
-			when :pubDate, :lastBuildDate, :published, :updated, :expirationDate, :modified, :'dc:date'
-				Time.parse(content) rescue unescape(content)
-			when :author, :contributor, :skipHours, :skipDays
-				unescape(content.gsub(/<.*?>/,''))
-			else
-				content.empty? && "#{attrs} " =~ /href=['"]?([^'"]*)['" ]/mi ? $1.strip : unescape(content)
-		end
-	end
+  end
 
-	def clean_tag(tag)
-		tag.to_s.gsub(':','_').intern
-	end
-	
+  def clean_content(tag, attrs, content)
+    content = content.to_s
+    case tag
+      when :pubDate, :lastBuildDate, :published, :updated, :expirationDate, :modified, :'dc:date'
+        Time.parse(content) rescue unescape(content)
+      when :author, :contributor, :skipHours, :skipDays
+        unescape(content.gsub(/<.*?>/,''))
+      else
+        content.empty? && (m = /href=['"]?([^'"]*)['" ]/mi.match("#{attrs} ") ) ? m[1].strip : unescape(content)
+    end
+  end
+
+  def clean_tag(tag)
+    tag.to_s.gsub(':','_').intern
+  end
+
   def unescape(content)
-  	if content =~ /([^-_.!~*'()a-zA-Z\d;\/?:@&=+$,\[\]]%)/n then
-  		CGI.unescape(content).gsub(/(<!\[CDATA\[|\]\]>)/,'').strip
-  	else
-  		content.gsub(/(<!\[CDATA\[|\]\]>)/,'').strip
-  	end
+    if content =~ /([^-_.!~*'()a-zA-Z\d;\/?:@&=+$,\[\]]%)/n then
+      CGI.unescape(content).gsub(/(<!\[CDATA\[|\]\]>)/,'').strip
+    else
+      content.gsub(/(<!\[CDATA\[|\]\]>)/,'').strip
+    end
   end
 end
 
--- simple-rss.gemspec
+++ simple-rss.gemspec
@@ -1,12 +1,12 @@
 Gem::Specification.new do |s|
   s.name = "simple-rss"
-  s.version = "1.2.3"
-  s.date = "2009-10-03"
+  s.version = "1.2.4"
+  s.date = "2010-01-13"
   s.summary = "A simple, flexible, extensible, and liberal RSS and Atom reader for Ruby. It is designed to be backwards compatible with the standard RSS parser, but will never do RSS generation."
-  s.email = "apdavidson@gmail.com"
-  s.homepage = "http://github.com/aldavidson/simple-rss"
+  s.email = "abintalal@gmail.com"
+  s.homepage = "http://github.com/aqab/simple-rss"
   s.description = "A simple, flexible, extensible, and liberal RSS and Atom reader for Ruby. It is designed to be backwards compatible with the standard RSS parser, but will never do RSS generation."
   s.has_rdoc = true
-  s.authors = ["Lucas Carlson", "Al Davidson"]
+  s.authors = ["Lucas Carlson", "Al Davidson", "Aqab Bin Talal"]
   s.files = ["install.rb", "lib", "lib/simple-rss.rb", "LICENSE", "Rakefile", "README", "simple-rss.gemspec", "test", "test/base", "test/base/base_test.rb", "test/data", "test/data/atom.xml", "test/data/not-rss.xml", "test/data/rss09.rdf", "test/data/rss20.xml", "test/data/blogspot_atom.xml", "test/test_helper.rb"]
 end
